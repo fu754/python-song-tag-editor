a4fb12ba2671790f75da4dde487c973ca2c03340
fu754 <admin@frozenutopia.net>
2024-04-28 23:28:03 +0900

    add comment

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from mutagen.id3 import ID3
 from mutagen.mp4 import MP4
 from typing import Final
 
+# 読み込み対象のディレクトリ
 DIRECTORY: Final[str] = 'songs'
 
 class FileInfo:
+    """
+    ファイルの情報
+    """
     file_path: str
     extension: str
 
@@ -14,6 +18,10 @@ class FileInfo:
         self.extension = extension
 
 def main() -> None:
+    """
+    メインの処理
+    """
+    # DIRECTORYディレクトリ以下のファイルパスと拡張子の取得
     file_info_list: list[FileInfo]= []
     for file_path in glob.glob(f'{DIRECTORY}/*.*', recursive=True):
         extension: str = file_path.split('.')[-1]
@@ -23,6 +31,7 @@ def main() -> None:
         )
         file_info_list.append(info)
 
+    # タグの表示
     for file_info in file_info_list:
         print(f'-------- Current file: {file_info.file_path}')
         if file_info.extension == 'mp3':
